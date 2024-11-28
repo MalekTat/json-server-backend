2ee8613c4903ff13a192ecd9afdc98b47ee75fe3
Malek T <malek_tatin@yahoo.com>
2024-11-28 14:15:41 +0100

    update

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 require("dotenv").config();
 const jsonServer = require("json-server");
 const morgan = require("morgan");
-
+const cors = require('cors')
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const PORT = process.env.PORT || 5005;
 
+server.use(cors());
 server.use(middlewares);
 server.use(morgan("dev"));
-server.use((req, res, next) => {
+/*server.use((req, res, next) => {
   // Middleware to disable CORS
   res.header("Access-Control-Allow-Origin", "*");
   next();
-});
+}); */
 server.use(router);
 
 server.listen(PORT, () => {
