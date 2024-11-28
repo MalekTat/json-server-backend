2215e675e150873b7f535f08a41b0f540e04b532
Malek T <malek_tatin@yahoo.com>
2024-11-28 15:45:50 +0100

    update

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ const morgan = require("morgan");
 
 // create two variables, path and fs
 //path is a tool that has the .join method to join paths from the operating system to the another string
-const path = require("path");
+    //const path = require("path");
 //fs is short for file system. Best metaphor is a librarian
-const fs = require("fs");
+    //const fs = require("fs");
 
 //const cors = require('cors')
 const server = jsonServer.create();
@@ -17,7 +17,7 @@ const server = jsonServer.create();
 
 //__dirname is the absolute path from the computers root directory to the current location
 
-const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
+    //const db = JSON.parse(fs.readFileSync(path.join(__dirname, "db.json")));
 
 
 const router = jsonServer.router("db.json");
@@ -32,8 +32,20 @@ server.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   next();
 }); 
+
+
+server.use(jsonServer.rewriter({
+  '/api/*': '/$1',
+  '/blog/:resource/:id/show': '/:resource/:id'
+}))
+
+
 server.use(router);
 
 server.listen(PORT, () => {
   console.log(`JSON Server is running at port ${PORT}`);
 });
+
+
+// Export the Server API
+module.exports = server
